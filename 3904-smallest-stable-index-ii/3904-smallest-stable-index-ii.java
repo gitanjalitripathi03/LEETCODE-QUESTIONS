c7061b1ef12e7082341a9292f21a343639175e54
Gitanjali Tripathi <gitanjalitripathi03@gmail.com>
2026-09-05 09:29:17 +0530

    Time: 5 ms (66.41%), Space: 126.3 MB (94.27%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -5,13 +5,14 @@ class Solution {
 
 
         int []maxarr = new int[n];
-        int []minarr = new int[n];
 
         int max = Integer.MIN_VALUE;
         for(int i=0; i<n; i++){
             max = Math.max(max, nums[i]);
             maxarr[i] = max;
         }
+
+        int []minarr = new int[n];
 
         int min = Integer.MAX_VALUE;
         for(int i=n-1; i>=0; i--){
